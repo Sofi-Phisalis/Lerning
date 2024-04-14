391f622a4fdc116b5d66d5b7dc5d10bbcc0bda5f
@valerii_sinitskii <valeriysinitskiy@yandex.ru>
2024-04-14 20:27:26 +0300

    hi

--- vk_cource/module_1/lasson2.py
+++ vk_cource/module_1/lasson2.py
@@ -1,6 +1,17 @@
-print("Привет")
 my = input()
-print("Hello,",my)
+print("Hello,", my)
+
+# С версии питона 3.0 появился метод .format() который позволяет вставлять переменные в тест:
+print("Hello, {}".format(my))
+# С версии питона 3.6 появились f строки. Они быстрее и удобнее:
+print(f"Hello, {my}")
+# Так же работает с несколькими переменными
 location = input()
 time = input()
-print("Current location is",location,"and time is",time)
+print("Current location is", location, "and time is", time)
+print("Current location is {} and time is {}".format(location, time))
+print(f"Current location is {location} and time is {time}")
+
+# А еще вызовы функций сразу можно вставлять в print
+print(f"Current location is {input()} and time is {input()}")
+
