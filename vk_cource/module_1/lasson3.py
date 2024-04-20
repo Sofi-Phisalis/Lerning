7dad05dfbb3f48f7b885b117d83cda3b3faa3547
s.anisimova <mreyv@mail.ru>
2024-04-20 19:15:20 +0300

    lesson 3

--- vk_cource/module_1/lasson3.py
+++ vk_cource/module_1/lasson3.py
@@ -1,12 +1,16 @@
-a1 = int(input())
-a2 = int(input())
-flag = True
+# a1 = int(input())
+# a2 = int(input())
+# flag = True
+#
+# while True:
+#    b3 = input()
+#    if not b3:
+#        break
+#    if not a1 <= int(b3) <= a2:
+#         flag = False
+#
+#  print(flag)
 
-while True:
-    b3 = input()
-    if not b3:
-        break
-    if not a1 <= int(b3) <= a2:
-        flag = False
+s = input()
+print(("a" in s or "o" in s) and "i" not in s and "e" not in s)
 
-print(flag)
